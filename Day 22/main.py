684138b8485ccb8233abafad6c6d202bc1a992b8
David Vance <davidvance512@gmail.com>
2024-12-24 15:26:17 -0500

    next commit

--- Day 22/main.py
+++ Day 22/main.py
@@ -1,8 +1,12 @@
-FILENAME = "sample_input.txt"
-#FILENAME = "input.txt"
+#FILENAME = "sample_input.txt"
+FILENAME = "input.txt"
+#FILENAME = "sample2.txt"
+
+import time
+from collections import defaultdict
 
 def main():
-
+    start = time.time()
     data = parse_data()
 
     answer1 = part1(data)
@@ -15,20 +19,53 @@ def main():
     print("--------Part 2 Answer-------------")
     print(answer2)
     print()
+    print(f"Total time: {time.time()-start}")
 
 
 def parse_data():
     with open(FILENAME, "r") as f:
         data = f.readlines()
-    return data
+    first_numbers = [int(row.strip()) for row in data]
+    return first_numbers
 
 
 def part1(data):
-    return None
+    two_thousands = []
+    for x in data:
+        for _ in range(2000):
+            first = x * 64
+            x = (first ^ x) % 16777216
+            second = x // 32
+            x = (second ^ x) % 16777216
+            third = x * 2048
+            x = (third ^ x) % 16777216
+        two_thousands.append(x)
+
+    return sum(two_thousands)
 
 
 def part2(data):
-    return None
+    counter = defaultdict(int)
+    for x in data:
+        changes = []
+        seen_keys = set()
+        for i in range(2000):
+            start_price = int(str(x)[-1])
+            first = x * 64
+            x = (first ^ x) % 16777216
+            second = x // 32
+            x = (second ^ x) % 16777216
+            third = x * 2048
+            x = (third ^ x) % 16777216
+            end_price = int(str(x)[-1])
+            changes.append((end_price-start_price))
+            if i > 2:
+                if (changes[-4], changes[-3], changes[-2], changes[-1]) not in seen_keys:
+                    counter[(changes[-4], changes[-3], changes[-2], changes[-1])] += end_price
+                    seen_keys.add((changes[-4], changes[-3], changes[-2], changes[-1]))
+
+
+    return max(counter.values())
 
 
 if __name__ == "__main__":
--- Day 24/main.py
+++ Day 24/main.py
@@ -1,12 +1,17 @@
-FILENAME = "sample_input.txt"
-#FILENAME = "input.txt"
+#FILENAME = "sample_input.txt"
+FILENAME = "input.txt"
+
+from collections import deque
+import time
 
 def main():
 
-    data = parse_data()
+    wire_values, instructions = parse_data()
 
-    answer1 = part1(data)
-    answer2 = part2(data)
+    part2instructions = instructions.copy()
+
+    answer1, binary = part1(wire_values, instructions)
+    answer2 = part2(wire_values, part2instructions, binary)
 
     print()
     print("--------Part 1 Answer-------------")
@@ -17,17 +22,129 @@ def main():
     print()
 
 
-def parse_data():
+def parse_data():   
     with open(FILENAME, "r") as f:
         data = f.readlines()
-    return data
+
+    wire_values = {}
+    instructions = deque()
+    for row in data:
+        if len(row.strip()) == 0:
+            continue
+        elif len(row.strip()) == 6:
+            wire, value = row.strip().split(": ")
+            wire_values[wire] = value
+        else:
+            first, operation, second, _, output = row.strip().split(" ")
+            instructions.append((first, second, output, operation))
+    
+    # print(wire_values)
+    # print()
+    # print(instructions)
+
+    return wire_values, instructions
 
 
-def part1(data):
-    return None
+def part1(wire_values, instructions):
+
+    while instructions:
+        next = instructions.popleft()
+        if next[0] in wire_values and next[1] in wire_values:
+            in1 = wire_values[next[0]]
+            in2 = wire_values[next[1]]
+            out_wire = next[2]
+            op = next[3]
+            # print(f"{in1=}, {in2=}, both in wire_values")
+            if op == "AND":
+                if in1 == "1" and in2 == "1":
+                    out = "1"
+                else:
+                    out = "0"
+            elif op == "OR":
+                if in1 == "1" or in2 == "1":
+                    out = "1"
+                else:
+                    out = "0"
+            else:
+                if in1 != in2:
+                    out = "1"
+                else:
+                    out = "0"
+            wire_values[out_wire] = out
+        else:
+            # print(f"{next=}")
+            instructions.append(next)
+        
+        # print(f"{wire_values=}")
+        # print()
+        # print(f"{instructions=}")
+        # print()
+        # print()
+
+    sorted_wires = {key: wire_values[key] for key in sorted(wire_values)}
+    zs = []
+    for wire in sorted_wires:      
+        if wire.startswith("z"):
+            zs.append(wire)
+    
+    # print(f"{sorted_wires=}")
+    # print(f"{zs=}")
+    
+    binary = "".join([wire_values[x] for x in zs[::-1]])
+    # print(binary)
+    return int(binary,2), binary
 
 
-def part2(data):
+def part2(wire_values, instructions, part1answer):
+    sorted_wires = {key: wire_values[key] for key in sorted(wire_values)}
+    xs = []
+    for wire in sorted_wires:      
+        if wire.startswith("x"):
+            xs.append(wire)
+    binaryx = "".join([wire_values[x] for x in xs[::-1]])
+
+    ys = []
+    for wire in sorted_wires:      
+        if wire.startswith("y"):
+            ys.append(wire)
+    binaryy = "".join([wire_values[x] for x in ys[::-1]])
+
+    decimal_x = int(binaryx, 2)
+    decimal_y = int(binaryy, 2)
+    expected_z = decimal_x + decimal_y
+    print(f"Expected z: {expected_z}")
+    print(f"Actual   z: {int(part1answer,2)}")
+    print()
+    binary_expected_z = bin(expected_z)[2:]
+    print(f"Expected binary z: {binary_expected_z}")
+    print(f"Part 1 answer   z: {part1answer}")
+    print()
+    
+    binary_length = len(part1answer)
+
+    wrong_bits = []
+    for i, bit in enumerate(binary_expected_z):
+        if part1answer[i] != bit:
+            wrong_bits.append((binary_length-i-1, bit, part1answer[i]))
+    
+    print(f"{wrong_bits=}")
+    print()
+
+    potentials = []
+    for wrong_bit, expected_bit, part1bit in wrong_bits:
+        for instruction in instructions:
+            if instruction[2] == "z" + str(wrong_bit) or instruction[2] == "z0" + str(wrong_bit):
+                potentials.append((instruction, expected_bit, part1bit))
+
+    print(potentials)
+    print()
+
+    for instruction, expected_bit, part1bit in potentials[::-1]:
+        print(f"{instruction=}")
+        print(f"{expected_bit=}")
+        print(f"{part1bit=}")
+        print()
+
     return None
 
 
