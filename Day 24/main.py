2ef4c8b3d4af759dc1e35daeaca4a992c2c45ade
David Vance <davidvance512@gmail.com>
2025-01-16 09:38:29 -0500

    Day 24 done

--- Day 24/main.py
+++ Day 24/main.py
@@ -3,26 +3,31 @@ FILENAME = "input.txt"
 
 from collections import deque
 import time
+import json
 
 def main():
     start = time.time()
 
     wire_values, instructions = parse_data()
+    parse_time = time.time()
 
     part2instructions = instructions.copy()
 
     answer1, binary = part1(wire_values, instructions)
+    part1_time = time.time()
     answer2 = part2(wire_values, part2instructions, binary)
+    part2_time = time.time()
 
+    print("------------------------------------")
+    print(f"Part 1 Answer: {answer1}")
     print()
-    print("--------Part 1 Answer-------------")
-    print(answer1)
+    print(f"Part 2 Answer: {answer2}")
     print()
-    print("--------Part 2 Answer-------------")
-    print(answer2)
-    print()
-    print(f"Execution took {1000*(time.time()-start)} ms.")
-    print()
+    print(f"Data Parse Execution Time:  {1000*(parse_time - start):.3f} ms")
+    print(f"Part 1 Execution Time:      {1000*(part1_time - parse_time):.3f} ms")
+    print(f"Part 2 Execution Time:      {1000*(part2_time - part1_time):.3f} ms")
+    print(f"Total Execution Time:       {1000*(part2_time - start):.3f} ms")
+    print("------------------------------------")
 
 
 
@@ -41,10 +46,6 @@ def parse_data():
         else:
             first, operation, second, _, output = row.strip().split(" ")
             instructions.append((first, second, output, operation))
-    
-    # print(wire_values)
-    # print()
-    # print(instructions)
 
     return wire_values, instructions
 
@@ -58,7 +59,6 @@ def part1(wire_values, instructions):
             in2 = wire_values[next[1]]
             out_wire = next[2]
             op = next[3]
-            # print(f"{in1=}, {in2=}, both in wire_values")
             if op == "AND":
                 if in1 == "1" and in2 == "1":
                     out = "1"
@@ -76,14 +76,7 @@ def part1(wire_values, instructions):
                     out = "0"
             wire_values[out_wire] = out
         else:
-            # print(f"{next=}")
             instructions.append(next)
-        
-        # print(f"{wire_values=}")
-        # print()
-        # print(f"{instructions=}")
-        # print()
-        # print()
 
     sorted_wires = {key: wire_values[key] for key in sorted(wire_values)}
     zs = []
@@ -91,26 +84,43 @@ def part1(wire_values, instructions):
         if wire.startswith("z"):
             zs.append(wire)
     
-    # print(f"{sorted_wires=}")
-    # print(f"{zs=}")
-    
     binary = "".join([wire_values[x] for x in zs[::-1]])
-    # print(binary)
     return int(binary,2), binary
+
+def build_tree(instructions, start_bit):
+        
+        if start_bit.startswith("x") or start_bit.startswith("y"):
+            return {"name": start_bit, "operation": None, "inputs": []}
+
+        for first, second, output, operation in instructions:
+            if start_bit == output:
+                first_branch = build_tree(instructions, first)
+                second_branch = build_tree(instructions, second)
+
+                return {
+                "name": start_bit,
+                "operation": operation,
+                "inputs": [first_branch, second_branch]
+            }
+        
+        return {"name": start_bit, "operation": None, "inputs": []}
+
+    
+
+
 
 
 def part2(wire_values, instructions, part1answer):
     sorted_wires = {key: wire_values[key] for key in sorted(wire_values)}
     xs = []
+    ys = []
     for wire in sorted_wires:      
         if wire.startswith("x"):
             xs.append(wire)
-    binaryx = "".join([wire_values[x] for x in xs[::-1]])
-
-    ys = []
-    for wire in sorted_wires:      
         if wire.startswith("y"):
             ys.append(wire)
+
+    binaryx = "".join([wire_values[x] for x in xs[::-1]])     
     binaryy = "".join([wire_values[x] for x in ys[::-1]])
 
     decimal_x = int(binaryx, 2)
@@ -121,7 +131,7 @@ def part2(wire_values, instructions, part1answer):
     print()
     binary_expected_z = bin(expected_z)[2:]
     print(f"Expected binary z: {binary_expected_z}")
-    print(f"Part 1 answer   z: {part1answer}")
+    print(f"Actual binary   z: {part1answer}")
     print()
     
     binary_length = len(part1answer)
@@ -129,28 +139,30 @@ def part2(wire_values, instructions, part1answer):
     wrong_bits = []
     for i, bit in enumerate(binary_expected_z):
         if part1answer[i] != bit:
-            wrong_bits.append((binary_length-i-1, bit, part1answer[i]))
+            wrong_bits.append("z" + (2-len(str(binary_length-i-1)))*"0" + str(binary_length-i-1))
     
     print(f"{wrong_bits=}")
     print()
 
-    potentials = []
-    for wrong_bit, expected_bit, part1bit in wrong_bits:
-        for instruction in instructions:
-            if instruction[2] == "z" + str(wrong_bit) or instruction[2] == "z0" + str(wrong_bit):
-                potentials.append((instruction, expected_bit, part1bit))
+    map = {}
+    for bit in wrong_bits:
+        path = build_tree(instructions, bit)
+        map[bit] = path
 
-    print(potentials)
-    print()
+    # output_file = "circuit_tree.json"
+    # with open(output_file, "w") as file:
+    #     json.dump(map["z33"], file, indent=4)
 
-    for instruction, expected_bit, part1bit in potentials[::-1]:
-        print(f"{instruction=}")
-        print(f"{expected_bit=}")
-        print(f"{part1bit=}")
-        print()
 
     return None
 
 
 if __name__ == "__main__":
     main()
+
+
+
+
+# bgs,pqc,rjm,swt,wsv,z07,z13,z31 - Final correct answer
+# Visualized the input with this awesome tool https://lukas.fyi/day24/
+# the code for part 2 above does not come close to solving the problem
